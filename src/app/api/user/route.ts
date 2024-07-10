1d0331c95eb7ae61018565846e9329bf925c0a3f
Laptop Wala <muhammadomairakbar@gmail.com>
2024-07-10 14:42:03 +0500

    deploying

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
+// import { PrismaClient } from '@prisma/client';
+// import { NextResponse } from 'next/server';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function POST(req: any) {
-  const { age, name, gender } = await req.json();
+// export async function POST(req: any) {
+//   const { age, name, gender } = await req.json();
 
-console.log(name,age,gender)
-  await prisma.user.create({
-    data: {
-      name,
-      age,
-      gender,
-    },
-  });
+// console.log(name,age,gender)
+//   await prisma.user.create({
+//     data: {
+//       name,
+//       age,
+//       gender,
+//     },
+//   });
 
-  return NextResponse.json({ success: true });
-}
+//   return NextResponse.json({ success: true });
+// }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-import { NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+// import { PrismaClient } from '@prisma/client';
+// import { NextApiResponse } from 'next';
+// import { NextResponse } from 'next/server';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function GET(req: any,res: NextApiResponse) {
+// export async function GET(req: any,res: NextApiResponse) {
 
- const users = await prisma.user.findMany()
+//  const users = await prisma.user.findMany()
 
-  return NextResponse.json({ success: true });
-}
+//   return NextResponse.json({ success: true });
+// }
